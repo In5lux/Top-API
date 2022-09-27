01c6a9ed001e388ee90a5dbd1297c1ac1a2c4afe
In5lux <nikos_almeryda@mail.ru>
2022-09-27 07:55:38 +0300

    Models

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { TopApiModule } from './top-api/top-api.module';
+import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [AuthModule, TopApiModule, ProductModule, ReviewModule],
+  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
