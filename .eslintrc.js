afc4e31ebf6d0521e2784ad1bbb4421718afff35
In5lux <nikos_almeryda@mail.ru>
2022-10-06 23:01:48 +0300

    Review service

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,12 +15,12 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ["node_modules", '.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-	'allowIndentationTabs': true
+		'allowIndentationTabs': true
   },
 };
